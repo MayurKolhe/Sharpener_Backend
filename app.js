a6626e189391570779b8e1ef06d8f5dbff05c122
“Mayur <mayurkolhe1995@csu.fullerton.edu>
2024-01-09 23:40:17 -0800

    Done with Get and Post in Express

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the Middleware");
-    next();
+app.use(bodyParser.urlencoded({extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+    res.send(
+      '<form action= "/product" method="post"><input type="text" name = "title"/><input type="Number" name = "size"/><button type="submit">Add Product</button></form>'
+    );
 })
 
-app.use((req, res, next) => {
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+
+app.use('/',(req, res, next) => {
     console.log("In another MiddleWare..!!")
     res.send('<h1> Hello Welcome Express JS');
 })
