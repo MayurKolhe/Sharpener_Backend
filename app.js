94857eca225e33d264389b1fc4d12a653b0532ea
“Mayur <mayurkolhe1995@csu.fullerton.edu>
2024-02-01 14:52:56 -0800

    comiiitng changes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const sequelize = require("./util/db");
 
@@ -19,14 +21,37 @@ const { error } = require("console");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((error) => console.log(error));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: false, onDelete: "CASCADE" });
+
+User.hasMany(Product);
+
 sequelize
   .sync()
-  .then(() => {
+  .then((result) => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ id: 1, name: "Max", email: "user1@test.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    console.log(user);
     app.listen(3000);
   })
   .catch((error) => {
