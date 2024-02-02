d5169b7c332f9994910d9a9a507647a9caed0d3d
“Mayur <mayurkolhe1995@csu.fullerton.edu>
2024-02-01 19:34:41 -0800

    commmiting cart Functionality

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const { error } = require("console");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -35,23 +36,33 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: false, onDelete: "CASCADE" });
-
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
 
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Product.belongsToMany(Cart, { through: CartItem });
+Cart.belongsToMany(Product, { through: CartItem });
+
+
 sequelize
-  .sync()
+  .query("SET FOREIGN_KEY_CHECKS = 0", { raw: true })
+  .then(() => {
+    sequelize.sync();
+  })
   .then((result) => {
     return User.findByPk(1);
   })
   .then((user) => {
     if (!user) {
-      return User.create({ id: 1, name: "Max", email: "user1@test.com" });
+      return User.create({name: "Max", email: "user1@test.com" });
     }
     return user;
   })
   .then((user) => {
-    console.log(user);
+    return user.createCart();
+  }).then(() => {
     app.listen(3000);
   })
   .catch((error) => {
--- controllers/shop.js
+++ controllers/shop.js
@@ -17,13 +17,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findByPk(prodId).then(product => {
-    res.render("shop/product-detail", {
-      product: product,
-      pageTitle: product.title,
-      path: "/products",
-    });
-  }).catch(error => console.log(error));
+  Product.findByPk(prodId)
+    .then((product) => {
+      res.render("shop/product-detail", {
+        product: product,
+        pageTitle: product.title,
+        path: "/products",
+      });
+    })
+    .catch((error) => console.log(error));
 };
 
 exports.getIndex = (req, res, next) => {
@@ -41,19 +43,67 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
-    path: "/cart",
-    pageTitle: "Your Cart",
-  });
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart
+        .getProducts()
+        .then((products) => {
+          res.render("shop/cart", {
+            path: "/cart",
+            pageTitle: "Your Cart",
+            products: products,
+          });
+        })
+        .catch((error) => console.log(error));
+    })
+    .catch((error) => console.log(error));
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect("/cart");
+  let fecthedCart;
+  let newQuantity = 1;
+  req.user
+    .getCart()
+    .then((cart) => {
+      fecthedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then((products) => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+      if (product) {
+        const oldquantity = product.cartItem.quantity;
+        newQuantity += oldquantity;
+        return product;
+      }
+      return Product.findByPk(prodId);
+    })
+    .then((product) => {
+      return fecthedCart.addProduct(product, {
+        through: { quantity: newQuantity },
+      });
+    })
+    .then(() => {
+      res.redirect("/cart");
+    })
+    .catch((error) => console.log(error));
 };
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user.getCart().then(cart => {
+    return cart.getProducts({ where: { id: prodId } });
+  }).then(products => {
+    const product = products[0];
+    return product.cartItem.destroy();
+  }).then(result => {
+    res.redirect("/cart");
+  }).catch(error => console.log(error));
+}
 
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
